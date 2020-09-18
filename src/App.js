7b76c817196eb8fb0a3d2e8d44d74d4a5fe943b7
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-18 10:25:20 -0700

    Installed cypress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Home from "./Home"
 import Pizza from "./Pizza";
--- src/formSchema.js
+++ src/formSchema.js
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 export default yup.object().shape({
     name: yup.string()
-        .required('Name is required').min(2, "Name is shorter than you are"),
+        .required('Name is required').min(2, "Name must be at least 2 characters"),
     size: yup.string(),
     instructions: yup.string(),
     pepperoni: yup.string(),
